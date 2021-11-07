089c8913c5da73cd3a47fb34f1dac19a96d46ae4
User <User@DESKTOP-PUEKB8G>
2021-11-07 18:50:25 +0000

    Kata 4 version 2

--- src/main/main.java
+++ src/main/main.java
@@ -9,13 +9,33 @@ import view.MailHistogramBuilder;
 import view.MailListReader;
 
 public class main {
+    
+    private static String fileName;
+    private static List<Mail> mailList;
+    private static Histogram<String> histogram;
+    
 
     public static void main(String[] args) {
-        String fileName = "C:\\Users\\Cecilia Gimenez\\Documents\\NetBeansProjects\\Kata4\\email.txt";
-        List<Mail> mailList = MailListReader.read(fileName);
-        Histogram<String> histogram = MailHistogramBuilder.build(mailList);
-        HistogramDisplay histoDisplay = new HistogramDisplay(histogram);
-        histoDisplay.execute();
+        fileName = "C:\\Users\\Cecilia Gimenez\\Documents\\NetBeansProjects\\Kata4\\email.txt";
+        execute();
+    }
+    
+    private static void execute(){
+        input();
+        process();
+        output();
+    }
+    
+    private static void input(){
+        mailList = MailListReader.read(fileName);
+    }
+    
+    private static void process(){
+        histogram = MailHistogramBuilder.build(mailList);
+    }
+    
+    private static void output(){
+        new HistogramDisplay(histogram).execute();
     }
     
 }
